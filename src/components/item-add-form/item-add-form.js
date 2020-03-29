2c2b85f1dcaf7d2c6446414c005fe392aebe1a7a
Roman Konashenkov <romash@Romans-MacBook.local>
2020-03-29 12:16:16 +0300

    new task label

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './item-add-form.css';
 
-const ItemAddForm = ({ addItem }) => {
-    return (
-        <div className="item-add-form">
-            <button className="btn btn-outline-secondary"
-                    type="button"
-                    onClick={() => addItem('Hello')}
-            >
-                Add item
-            </button>
-        </div>
-    )
-}
+export default class ItemAddForm extends Component {
 
-export default ItemAddForm;
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addItem(this.state.label);
+        this.setState({
+            label: ''
+        });
+    }
+
+    render() {
+        return (
+            <form className="item-add-form d-flex"
+                    onSubmit={this.onSubmit}>
+                <input className="form-control"
+                        type="text"
+                        placeholder="What needs to be done"
+                        value={this.state.label}
+                        onChange={this.onLabelChange}/>
+                <button className="btn btn-outline-secondary"
+                        type="submit">
+                    Add item
+                </button>
+            </form>
+        )
+    }
+}
